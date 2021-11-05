1d0223927283f4170f0dded99dc53133e149e5ac
LeviSilvaz99 <contatolevisilva@outlook.com>
2021-11-05 10:58:33 -0300

    Initial Commit

---
+++ BarGraphGestures/BarGraphGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarGraphGesturesApp.swift
+//  BarGraphGestures
+//
+//  Created by Levi  on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BarGraphGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarGraphGestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarGraphGestures
+//
+//  Created by Levi  on 03/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
